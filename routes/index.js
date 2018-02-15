3a26d826e0648bf0fd4e890cea35eff74174bead
renato niola <niolarenato01@gmail.com>
2018-02-15 11:23:52 +0100

    game working but without checks to win

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const games = require('./games')
+const users = require('./users')
+const sessions = require('./sessions')
+const gamePlayers = require('./games/players')
+const turns = require('./turns.js')
+
+module.exports = {
+  games,
+  users,
+  sessions,
+  gamePlayers,
+  turns,
+}
---
+++ routes/turns.js
@@ -0,0 +1,54 @@
+const router = require('express').Router()
+const passport = require('../config/auth')
+const { Game } = require('../models')
+
+const authenticate = passport.authorize('jwt', { session: false })
+
+module.exports = io => {
+  router
+  .post('/turns' , authenticate,(req, res, next) => {
+    console.log('accunt : ' , req.account)
+
+    const { gameId , location , userId } = req.body
+    Game.findById(gameId)
+      .then((game) => {
+
+      console.log('turn : ' , game.turn)
+
+      let playerIndex;
+      game.players.map( (item ,index) => {
+         if( item.userId == userId ) {
+            playerIndex = index
+         }
+      })
+
+      if( game.turn != playerIndex && game.grid[location] == 0) {
+
+        const newGrid = game.grid.slice()
+
+        newGrid[location] = userId
+
+        game.grid = newGrid
+        game.turn = playerIndex
+        console.log('ok your turn ')
+        game.save()
+
+        io.emit('action', {
+          type: 'GAME_UPDATED',
+          payload: game
+        })
+
+      }else{
+        console.log('not your turn ')
+        res.send({ message : 'not your turn'})
+      }
+
+      })
+      .catch((error) => next(error))
+
+
+
+  })
+
+  return router
+}
